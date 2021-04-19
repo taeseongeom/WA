51bae461ddd9e777dcab2db5a8cecb8566e3ecd7
Myoryung <iove_v@naver.com>
2021-04-19 10:08:49 +0900

    Add: 스테이지 초기화 기능 추가

--- Project/WA/Source/WA/WAGameModeBase.cpp
+++ Project/WA/Source/WA/WAGameModeBase.cpp
@@ -1,5 +1,23 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
-
 #include "WAGameModeBase.h"
 
+AWAGameModeBase::AWAGameModeBase()
+{
+	CurrentRoomNum = 0;
+}
+
+void AWAGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWAGameModeBase::AddInitPuzzle(ADefaultPuzzle* value, int roomNum){
+	InitPuzzles.Add(roomNum, value);
+}
+
+void AWAGameModeBase::RoomReset(){
+	for (TMultiMap<int8, ADefaultPuzzle*>::TKeyIterator iter = InitPuzzles.CreateKeyIterator(CurrentRoomNum);
+		iter; ++iter)
+		iter.Value()->InitializePuzzle();
+}
