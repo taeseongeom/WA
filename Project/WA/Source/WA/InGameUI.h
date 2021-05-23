0313abac9085c3f5648027791725fa37a22e4147
Jinho Park <jinnho97@gmail.com>
2021-05-24 03:08:50 +0900

    Add: 인게임 UI 출력 기능 추가
    - 인게임 UI 관련 작업을 진행하는 UInGameUI 클래스 추가
    - 플레이어 캐릭터가 가진 정보를 출력하므로, 해당 정보를 동기화하는 기능 추가
    - 현재 플레이어 캐릭터가 UI를 소유하고 있으므로, 추후 GameModeBase로 옮기는 과정이 필요할 것으로 보임

---
+++ Project/WA/Source/WA/InGameUI.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EngineMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameUI.generated.h"
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class WA_API UInGameUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeOnInitialized() override;
+
+public:
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_portrait;
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_interactable;
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_health_0;
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_health_1;
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_health_2;
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_stamina;
+
+	UPROPERTY(Meta = (BindWidget))
+	UImage* img_dialog;
+	UPROPERTY(Meta = (BindWidget))
+	UTextBlock* txt_dialog;
+
+
+	void UpdateHealthBar(int HealthPoint);
+	void UpdateStamina(bool IsActive);
+
+	void DisplayText(const FString& String);
+};
