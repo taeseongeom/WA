fa19324178f668ee2666a98ac7d15c66214ca3e6
Jinho Park <jinnho97@gmail.com>
2021-06-20 01:22:32 +0900

    Add: 점프 폭탄 오브젝트 추가 (보스 4패턴)
    - JumpCount 횟수 점프하는 폭탄 추가
    - 점프 횟수와 이동 시간은 보스 패턴에서 설정 가능
    - 블루프린트에서 점프 높이 설정 가능
    - 별도의 폭발 판정이 필요하므로 BombRange 블루프린트 추가

---
+++ Project/WA/Source/WA/JumpingBomb.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JumpingBomb.h"
+
+#include "PlayerCharacter.h"
+
+
+AJumpingBomb::AJumpingBomb()
+{
+ 	PrimaryActorTick.bCanEverTick = true;
+
+	bombEffect = nullptr;
+
+	jumpHeight = 500.0f;
+
+	playerCharacter = nullptr;
+	startPos = FVector::ZeroVector;
+	targetPos = FVector::ZeroVector;
+	curTime = 0;
+	travelTime = 0;
+
+	jumpCount = 0;
+}
+
+void AJumpingBomb::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (jumpCount > 0)
+	{
+		curTime += DeltaTime;
+
+		float rate = curTime / travelTime;
+		if (rate >= 1)
+		{
+			GetWorld()->SpawnActor<AActor>(bombEffect, targetPos, FRotator::ZeroRotator);
+			startPos = targetPos;
+			targetPos = playerCharacter->GetActorLocation();
+			targetPos.Z = 0;
+			curTime = 0;
+
+			jumpCount--;
+		}
+
+		SetActorLocation(FMath::Lerp<FVector>(startPos, targetPos, rate) + FVector(0, 0, FMath::Sin(PI * rate)) * jumpHeight);
+	}
+	else
+	{
+		Destroy();
+	}
+}
+
+void AJumpingBomb::FireBomb(APlayerCharacter* PlayerCharacter, float TravelTime, int32 JumpCount)
+{
+	playerCharacter = PlayerCharacter;
+	travelTime = TravelTime;
+	jumpCount = JumpCount;
+
+	startPos = GetActorLocation();
+	targetPos = playerCharacter->GetActorLocation();
+	targetPos.Z = 0;
+	curTime = 0;
+}
